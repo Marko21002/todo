d07633e3f663ebf55e3d7709f9851ca269fa97cc
Marko21002 <markgufman21@gmail.com>
2023-12-29 03:31:06 +0100

    fix

--- app/actions/addTodo.tsx
+++ app/actions/addTodo.tsx
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 
 const prisma = new PrismaClient();
 
-export async function addTodo(formData) {
+export async function addTodo(formData: { get: (arg0: string) => any }) {
   const title = formData.get("grudgeName");
   const todo = await prisma.todo.create({
     data: {
